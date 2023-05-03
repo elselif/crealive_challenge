f0893a6c256841962330f55a05a4bfbf133c16ad
Elif <2003013054@student.beykent.edu.tr>
2023-05-03 17:07:31 +0300

    admins table , migration , make model and login, forget password blade page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,6 @@ Route::get('/', function () {
 // Admin
 
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin.home');
+Route::get('admin/login', [AdminLoginController::class, 'index'])->name('admin.login');
+Route::get('admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
+
