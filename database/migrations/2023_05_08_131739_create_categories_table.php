6aee254ef0804ce12de87e1fc9e7a31d5d84fc1e
Elif <2003013054@student.beykent.edu.tr>
2023-05-11 03:59:58 +0300

    Update 2023_05_08_131739_create_categories_table.php

--- database/migrations/2023_05_08_131739_create_categories_table.php
+++ database/migrations/2023_05_08_131739_create_categories_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category_name');
             $table->string('show_on_menu');
